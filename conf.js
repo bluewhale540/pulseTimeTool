5a93c4c169a174163bb71500ff9d72dfaeabf31a
Pham, Mathew <mpham@idirect.net>
2021-06-10 10:12:57 -0400

    refactored some variables for clarity

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ exports.config = {
     if (browser.params.ip == 'none' || browser.params.file == 'none') {
       console.log('\x1b[31m%s\x1b[0m', 'No IP address and/or file specified.');
       console.log('\x1b[36m%s\x1b[0m', `Parameters can be specified with "--params.parameter value" (no quotes):
-      --params.ip           the IP address
+      --params.ip           the web address of the pulse instance
       --params.file         the file to read from
       --params.outfile      the file to write to; if not specified, will write to the input file
       --params.username     the username to use on the login screen; default is admin
